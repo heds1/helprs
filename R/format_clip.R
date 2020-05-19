f945c2e80b49e57342d40cf4c2558b20eebc8339
heds1 <hedley.stirrat@gmail.com>
2020-05-19 20:30:12 +1200

    initial commit

---
+++ R/format_clip.R
@@ -0,0 +1,9 @@
+#' Convert backslashes to forward slashes directly from the clipboard.
+#'
+#' @return The string copied to the clipboard is returned with backslashes
+#replaced with forward slashes.
+#' @example format_clip()
+
+format_clip <- function() {
+    gsub("\\\\", "/", readClipboard())
+}
